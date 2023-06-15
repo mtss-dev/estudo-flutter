157c3722bce02b87a42538469e284c29799e58d9
mtss-dev <matheus.silva@icomp.ufam.edu.br>
2023-06-15 00:09:05 -0400

    Aula sobre Text e ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -16,62 +16,83 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
           useMaterial3: true,
         ),
-        home: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
+        home: Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.red,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.cyan,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.pinkAccent,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.purpleAccent,
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.amber,
+                height: 30,
+                width: 300,
+                child: const Text(
+                  "Hello World",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      decoration: TextDecoration.none),
                 ),
-                Container(
-                  height: 50,
-                  width: 50,
-                  color: Colors.blue,
-                ),
-              ],
-            ),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.blue,
-                ),
-                Container(
-                  height: 50,
-                  width: 50,
-                  color: Colors.red,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  height: 50,
-                  width: 50,
-                  color: Colors.cyan,
-                ),
-                Container(
-                  height: 50,
-                  width: 50,
-                  color: Colors.pinkAccent,
-                ),
-                Container(
-                  height: 50,
-                  width: 50,
-                  color: Colors.purpleAccent,
-                ),
-              ],
-            )
-          ],
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    print('Você apertou o botão');
+                  },
+                  child: const Text('Aperte-me')),
+            ],
+          ),
         ));
   }
 }
