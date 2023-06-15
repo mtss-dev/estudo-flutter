f1445086d6c8198983f1a7aa1faa5599137a6f62
mtss-dev <matheus.silva@icomp.ufam.edu.br>
2023-06-14 23:52:02 -0400

    Aula sobre Column e Row

--- lib/main.dart
+++ lib/main.dart
@@ -11,41 +11,67 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
-        useMaterial3: true,
-      ),
-      home: Stack(
-        alignment: AlignmentDirectional.center,
-        children: [
-          Container(
-            color: Colors.black,
-            width: 300,
-            height: 300,
-          ),
-          Container(
-            color: Colors.red,
-            width: 150,
-            height: 150,
-          ),
-          Container(
-            color: Colors.blue,
-            width: 75,
-            height: 75,
-          ),
-          Container(
-            color: Colors.yellow,
-            width: 37,
-            height: 37,
-          ),
-          Container(
-            color: Colors.green,
-            width: 18,
-            height: 18,
-          ),
-        ],
-      ),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+          useMaterial3: true,
+        ),
+        home: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.blue,
+                ),
+                Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.red,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.cyan,
+                ),
+                Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.pinkAccent,
+                ),
+                Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.purpleAccent,
+                ),
+              ],
+            )
+          ],
+        ));
   }
 }
